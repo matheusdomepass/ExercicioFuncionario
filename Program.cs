8777429c8063b5dbdfaeb5c9e13d26d43c47edc8
matheus.domeneghetti <matheus.domeneghetti@RPOLOG0046>
2024-06-14 10:48:49 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + funcionario);
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagemAumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagemAumento);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + funcionario);
+        }
+    }
+}
